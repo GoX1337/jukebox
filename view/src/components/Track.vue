fa6da0090cb3a401f7990bb69027aad54c8f78d9
gox <gox.roche@gmail.com>
2021-10-27 19:48:13 +0200

    main view

--- view/src/components/Track.vue
+++ view/src/components/Track.vue
@@ -1,5 +1,18 @@
 <template>
-    <h3>{{ index + " " + track.artists[0].name + " " + track.name}}</h3>
+    <div class="container">
+      <div class="Track">{{ index }}<strong class="artistName">{{ track.artists[0].name }}</strong>{{ track.name}}</div>
+      <div class="upvote">
+        <a href="#" v-on:click="upvote()">
+          <img class="arrow" src="@/assets/arrow.png" />
+        </a>
+      </div>
+      <div class="downvote">
+        <a href="#" v-on:click="downvote()">
+          <img class="arrow arrowDown" src="@/assets/arrow.png" />
+        </a>
+      </div>
+      <div class="votecount">{{ track.vote || 1 }}</div>
+    </div>
 </template>
 
 <script>
@@ -8,24 +21,76 @@ export default {
   props: {
     index: Number,
     track: Object
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+      upvote: function () {
+         console.log("upvote")
+      },
+      downvote: function () {
+         console.log("downvote")
+      }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.arrow {
+  height: 30px;
+  width: auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.arrowDown {
+  transform: rotateX(180deg);
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.artistName {
+  padding-left: 5px;
+  padding-right: 5px;
 }
-a {
-  color: #42b983;
+
+.container {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 30%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 0.8fr 1.4fr 0.8fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    ". . . . . . upvote"
+    "Track Track Track Track Track Track votecount"
+    ". . . . . . downvote";
+  margin-bottom: 15px;
+  border: solid;
+  border-width: 0.3px;
 }
+
+.Track { 
+  display: flex;
+  align-items: center;
+  text-align: left;
+  padding-left: 15px;
+  grid-area: Track; 
+  vertical-align: middle;
+}
+
+.upvote { grid-area: upvote; }
+
+.downvote { grid-area: downvote; }
+
+.votecount {
+  grid-area: votecount; 
+  display: flex;
+  align-items: center;
+  padding-left: 45%;
+}
+
 </style>
